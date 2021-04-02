be6f8fdb79021fda25f17e325e8937da83a7a945
timaldo <info@timdorsett.com>
2021-04-02 17:15:38 +0100

    added products page

---
+++ pages/products.js
@@ -0,0 +1,15 @@
+const Products = () => {
+  return (
+    <>
+      <h1>Cool Product 1</h1>
+      <p>This is a very cool product</p>
+      <p>£2.50</p>
+      <hr />
+      <h1>Cool Product 2</h1>
+      <p>This is an even cooler product</p>
+      <p>£200.50</p>
+    </>
+  );
+};
+
+export default Products;
